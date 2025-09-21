3ce85d8a5beafa9edf05929c212bc847da22bf4a
Berk <79381262+IDirkI@users.noreply.github.com>
2025-09-21 14:00:21 -0400

    Correct IK logic to account for servo height from world frame

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,9 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
 
 float angle2micros(float rad);
+void vLine();
+void hLine();
+void circle();
 
 constexpr float q1_0 = 0.0f;
 constexpr float q2_0 = M_PI_2;
@@ -28,10 +31,15 @@ constexpr float q2Max = M_PI_4;
 
 constexpr float L1 = 0.12f;
 constexpr float L2 = 0.13f;
+constexpr float h = 0.008f;
 
 Vector<2> x{L1, L2};
 Vector<2> q{};
 Vector<2> x_d{0.0f, 0.0f};
+
+float r = 0.04;
+Vector<2> center{0.0f, 0.17f};
+float t = 0;
 
 
 void setup() {
@@ -43,15 +51,15 @@ void setup() {
   pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
 
   pwm.writeMicroseconds(0, angle2micros(0));
-  pwm.writeMicroseconds(1, angle2micros(M_PI_4));
+  pwm.writeMicroseconds(1, angle2micros(0));
 
-  delay(20);
+  delay(200);
 }
 
 Vector<2> getQValues(const Vector<2> &x_d) {
     Vector<2> q;
-    q[1] = std::acos((x_d[0]*x_d[0] + x_d[1]*x_d[1] - L1*L1 - L2*L2)/(2*L1*L2));
-    q[0] = std::atan2(x_d[1], x_d[0]) - std::atan2(L2*std::sin(q[1]), L1 + L2*std::cos(q[1]));
+    q[1] = std::acos((x_d[0]*x_d[0] + x_d[1]*x_d[1] - L1*L1 - L2*L2 - h*h)/(2*L1*L2*std::sqrt(1+(h*h)/(L1*L1)))) + std::atan(h/L1);
+    q[0] = std::atan2(x_d[1], x_d[0]) - std::atan2(L2*std::sin(q[1] - h), L1 + L2*std::cos(q[1]));
 
     // Conversion per my model and offsets
     q[0] *= -1;
@@ -65,6 +73,15 @@ Vector<2> getQValues(const Vector<2> &x_d) {
     return q;
 }
 
+void loop() {
+
+  for(int i = 0; i < 5; i++) { circle(); }
+
+  for(int i = 0; i < 3; i++) { hLine(); }
+
+  for(int i = 0; i < 3; i++) { vLine(); }
+}
+
 float angle2micros(float rad) {
   rad += M_PI_2;
 
@@ -74,9 +91,24 @@ float angle2micros(float rad) {
   return rad;
 }
 
-void loop() {
+void circle() {
+  while(t <= 2*M_PI) {
+    x_d[0] = 0.06*std::cos(t) + center.x();
+    x_d[1] = 0.06*std::sin(t) + center.y();
+    t += 0.05;
 
-  for(float i = 0.15f; i <= 0.20; i += 0.001) {
+    q = getQValues(x_d);
+    printf("[ %3.2f %3.2f ]\n", q[0], q[1]);
+    pwm.writeMicroseconds(0, angle2micros(q[0]));
+    pwm.writeMicroseconds(1, angle2micros(q[1]));
+  }
+  t = 0;
+}
+
+void vLine() {
+  x_d[0] = 0.0f;
+
+  for(float i = 0.11f; i <= 0.23; i += 0.002) {
     x_d[1] = i;
     q = getQValues(x_d);
     printf("[ %3.2f %3.2f ]\n", q[0], q[1]);
@@ -84,12 +116,31 @@ void loop() {
     pwm.writeMicroseconds(1, angle2micros(q[1]));
   }
 
-  for(float i = 0.20f; i >= 0.15; i -= 0.001) {
+  for(float i = 0.23f; i >= 0.11; i -= 0.002) {
     x_d[1] = i;
     q = getQValues(x_d);
     printf("[ %3.2f %3.2f ]\n", q[0], q[1]);
     pwm.writeMicroseconds(0, angle2micros(q[0]));
     pwm.writeMicroseconds(1, angle2micros(q[1]));
   }
+}
 
+void hLine() {
+  x_d[1] = 0.12f;
+
+  for(float i = 0.12f; i >= -0.12; i -= 0.003) {
+    x_d[0] = i;
+    q = getQValues(x_d);
+    printf("[ %3.2f %3.2f ]\n", q[0], q[1]);
+    pwm.writeMicroseconds(0, angle2micros(q[0]));
+    pwm.writeMicroseconds(1, angle2micros(q[1]));
+  }
+
+  for(float i = -0.12f; i <= 0.12; i += 0.003) {
+    x_d[0] = i;
+    q = getQValues(x_d);
+    printf("[ %3.2f %3.2f ]\n", q[0], q[1]);
+    pwm.writeMicroseconds(0, angle2micros(q[0]));
+    pwm.writeMicroseconds(1, angle2micros(q[1]));
+  }
 }
